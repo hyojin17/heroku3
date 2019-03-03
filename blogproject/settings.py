ddb9346532a50a675c590128531d95e2418a8b3a
hyojin17 <chj7960@likelion.org>
2019-03-03 21:45:00 +0900

    헤로쿠배포전

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -67,13 +67,8 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER' : 'postgres',
-        'PASSWORD' : '#',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
