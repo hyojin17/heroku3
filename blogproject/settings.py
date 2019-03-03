01cf7e1b01b340113f5e006f927a831c838b7221
hyojin17 <chj7960@likelion.org>
2019-03-03 23:06:55 +0900

    제발되라1

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'et9e&%d7b#n$la59c!rgtdswms5&#3
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
